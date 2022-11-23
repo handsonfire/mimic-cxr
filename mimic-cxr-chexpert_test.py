595988eef87d869425a9100ed4c5c6527692d146
handsonfire <116737928+handsonfire@users.noreply.github.com>
2022-11-23 15:51:55 +0900

    Update mimic-cxr-chexpert_test.py

--- mimic-cxr-chexpert_test.py
+++ mimic-cxr-chexpert_test.py
@@ -23,10 +23,12 @@ test_df = pd.merge(ms_test, df_chextest, how='left', left_on='study_id_x', right
 # 중복된 내용 지우기, 인덱스 순서 정리하기, 결측값 표시를 0으로 바꾸기 등 chexpert 파일과 비슷하게 수정
 
 test_df.drop(['subject_id_x','study_id_x','split'], axis=1, inplace=True)
-
-test_df = test_df.reindex(['dicom_id','subject_id', 'study_id', 'No Finding', 'Enlarged Cardiomediastinum', 'Cardiomegaly', 'Lung Opacity', 'Lung Lesion', 'Edema', 'Consolidation', 'Pneumonia', 'Atelectasis', 'Pneumothorax', 'Pleural Effusion', 'Pleural Other', 'Fracture', 'Support Devices', 'ViewCodeSequence_CodeMeaning', 'ViewPosition', 'ProcedureCodeSequence_CodeMeaning'], axis='columns')
-
-test_df = test_df.fillna({'No Finding':0, 'Enlarged Cardiomediastinum':0,'Cardiomegaly':0,'Lung Opacity':0,'Lung Lesion':0,'Edema':0,'Consolidation':0,'Pneumonia':0,'Atelectasis':0,'Pneumothorax':0,'Pleural Effusion':0,'Pleural Other':0,'Fracture':0,'Support Devices':0,})
+test_df = test_df.reindex(['dicom_id','subject_id', 'study_id', 'No Finding', 'Enlarged Cardiomediastinum', 'Cardiomegaly', 'Lung Opacity', 'Lung Lesion',
+                           'Edema', 'Consolidation', 'Pneumonia', 'Atelectasis', 'Pneumothorax', 'Pleural Effusion', 'Pleural Other', 'Fracture', 
+                           'Support Devices', 'ViewCodeSequence_CodeMeaning', 'ViewPosition', 'ProcedureCodeSequence_CodeMeaning'], axis='columns')
+test_df = test_df.fillna({'No Finding':0, 'Enlarged Cardiomediastinum':0,'Cardiomegaly':0,'Lung Opacity':0,
+                          'Lung Lesion':0,'Edema':0,'Consolidation':0,'Pneumonia':0,'Atelectasis':0,'Pneumothorax':0,'Pleural Effusion':0,
+                          'Pleural Other':0,'Fracture':0,'Support Devices':0,})
 
 
 # "ViewPosition" 인덱스 중 결측값에, 같은 행의"ProcedureCodeSequence_CodeMeaning"의 정보 입력하기
